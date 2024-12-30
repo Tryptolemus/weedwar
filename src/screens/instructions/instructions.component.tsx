27b744e433644056b7f81ff96db1d83fbfbfddc7
Tryptolemus <86113567+Tryptolemus@users.noreply.github.com>
2024-12-30 01:31:02 -0500

    Update instructions.component.tsx

--- src/screens/instructions/instructions.component.tsx
+++ src/screens/instructions/instructions.component.tsx
@@ -16,16 +16,18 @@ export default function Instructions() {
     <InstructionsContainer>
       <InstructionsTitleContainer>instructions</InstructionsTitleContainer>
       <div>
+        Burn 100 Gala To Begin the Game.  Climb the Leaderboard by January 30th to 
+        win a Connoisseur Node By MetaFlora
         You are a drug dealer engaged in arbitrage! This is a game of buying,
         selling, and fighting. The object of the game is to pay off your debt to
         the loan shark. Then, make as much money as you can in a 30 day period.
         It takes one day to travel to a new location. If you deal too heavily in
-        drugs, you might run into the police!! Buy guns to protect yourself from
-        muggers and police dogs. Your main drug stash will be in the Bronx.
+        weed, you might run into the police!! Buy guns to protect yourself from
+        muggers and police dogs. Your main stash will be in Grass Valley.
         (It's a nice neighborhood) Put money in the bank to earn interest, but
         be careful, the loan shark also collects interest.
       </div>
-      <div>The Prices of drugs per unit are:</div>
+      <div>The Prices of cannabis per unit are:</div>
       <PriceBox withDialog={false} priceRange={true} />
       <Button onClick={() => dispatch(updateActionEvent(ActionEvents.Start))}>
         back
